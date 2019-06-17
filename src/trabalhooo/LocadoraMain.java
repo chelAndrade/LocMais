ec5d5d79cb86b6af2e010be0d36399e742338e72
michel <>
2019-06-17 17:40:08 -0300

    ola mundo

--- src/trabalhooo/LocadoraMain.java
+++ src/trabalhooo/LocadoraMain.java
@@ -17,6 +17,7 @@ public class LocadoraMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("ola mundo");
        
     
 }
