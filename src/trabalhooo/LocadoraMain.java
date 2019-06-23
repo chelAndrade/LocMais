12f7844480334855e37e812a22db2d96374c74c6
chelAndrade <44234660+chelAndrade@users.noreply.github.com>
2019-06-22 23:19:39 -0300

    Primeira Tela Interface

--- src/trabalhooo/LocadoraMain.java
+++ src/trabalhooo/LocadoraMain.java
@@ -5,6 +5,10 @@
  */
 package trabalhooo;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 
 /**
@@ -17,8 +21,37 @@ public class LocadoraMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ola mundo");
+        //System.out.println("ola mundo");
+         JFrame frame = new JFrame("LocMais");
+         JPanel painel = new JPanel();
+        
+        
+        JButton botao = new JButton("Cadastrar Filmes");
        
+        botao.setToolTipText("Clique aqui para salvar");
+        painel.add(botao);
+        
+        JButton botao1 = new JButton("Cadastar Fornecedor");
+        
+        botao1.setToolTipText("Clique aqui para salvar");
+        painel.add(botao1);
+       
+        JButton botao2 = new JButton("Cadastrar Clientes");
+        
+        botao2.setToolTipText("Clique aqui para salvar");
+        painel.add(botao2);
+        
+        frame.getContentPane().add(new JButton("Teste")); 
+       
+        frame.getContentPane().add(painel);
+        
+        frame.setSize(400, 600);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        
+        
     
-}
+    }
 }
