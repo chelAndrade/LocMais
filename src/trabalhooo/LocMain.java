0fc121db98442a615abfdd71a35cb9c15bf06a75
chelAndrade <44234660+chelAndrade@users.noreply.github.com>
2019-06-25 00:59:09 -0300

    Add files via upload

---
+++ src/trabalhooo/LocMain.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhooo;
+
+/**
+ *
+ * @author miche
+ */
+public class LocMain {
+    public static void main(String[] args) {
+       System.out.println("ola mundo");
+       /*Filme acao = new FilmeAcao();
+       acao.setAnoLancamento((12/01/2011));
+       acao.setClassificacaoIndicativa("14");
+       acao.setQuantiDisponivel(4);
+       acao.setTitulo("Os Mercenarios");
+       acao.setValorAlocacao(3.5f);
+       acao.setValorCompra(2.5f);
+       
+       
+       Filme terror = new FilmeTerror();
+       terror.setAnoLancamento((17/01/2017));
+       terror.setClassificacaoIndicativa("18");
+       terror.setQuantiDisponivel(4);
+       terror.setTitulo("Anabelle");
+       terror.setValorAlocacao(3.9f);
+       terror.setValorCompra(2.9f);
+       
+       
+       Filme comedia = new FilmeComedia();
+       comedia.setAnoLancamento((22/05/1999));
+       comedia.setClassificacaoIndicativa("14");
+       comedia.setQuantiDisponivel(4);
+       comedia.setTitulo("American Pie");
+       comedia.setValorAlocacao(3.7f);
+       comedia.setValorCompra(2.2f);
+       
+       
+       Filme adulto = new FilmeAdulto();
+       adulto.setAnoLancamento((29/06/1999));
+       adulto.setClassificacaoIndicativa("18");
+       adulto.setQuantiDisponivel(4);
+       adulto.setTitulo("As Brasileirinhas");
+       adulto.setValorAlocacao(4.4f);
+       adulto.setValorCompra(3.0f);
+       
+       
+       Filme aventura = new FilmeAventura();
+       acao.setAnoLancamento((12/01/2011));
+       acao.setClassificacaoIndicativa("12");
+       acao.setQuantiDisponivel(4);
+       acao.setTitulo("Piratas do Caribe");
+       acao.setValorAlocacao(3.6f);
+       acao.setValorCompra(2.1f);*/
+       
+       
+    }
+    
+}
