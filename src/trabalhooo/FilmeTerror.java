ab62a9bee25351fed569ff7e3434b95a64c5a697
chelAndrade <44234660+chelAndrade@users.noreply.github.com>
2019-06-26 23:59:39 -0300

    Update FilmeTerror.java

--- src/trabalhooo/FilmeTerror.java
+++ src/trabalhooo/FilmeTerror.java
@@ -5,12 +5,18 @@
  */
 package trabalhooo;
 
+import java.util.Date;
+
 /**
  *
  * @author miche
  */
 public class FilmeTerror  extends Filme{
     private String classificacaoIndicativa;
+
+    public FilmeTerror( String titulo, Date anoLancamento, String classificacaoIndicativa, int quantiDisponivel, float valorAlocacao, float valorCompra) {
+        super( titulo, anoLancamento, classificacaoIndicativa, quantiDisponivel, valorAlocacao, valorCompra);
+    }
     
     /**
      * @return the classificacaoIndicativa
