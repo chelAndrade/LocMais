1712537f570ddb0a55e7decac65c8f9c1e43dee1
chelAndrade <44234660+chelAndrade@users.noreply.github.com>
2019-06-27 00:42:50 -0300

    Update LocMais.java

--- src/LocMais.java
+++ src/LocMais.java
@@ -489,7 +489,7 @@ public class LocMais extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        LocadoraMain.abreJanela();
+       
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
