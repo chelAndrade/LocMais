35e0ef837d15f193d00963904c73e6bcc6711ec9
chelAndrade <44234660+chelAndrade@users.noreply.github.com>
2019-06-30 16:51:15 -0300

    Update TelaPessoa.java

--- src/tela/TelaPessoa.java
+++ src/tela/TelaPessoa.java
@@ -68,6 +68,11 @@ public class TelaPessoa extends javax.swing.JFrame {
         jButton2.setText("Atualizar");
 
         jButton3.setText("Sair");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -146,6 +151,11 @@ public class TelaPessoa extends javax.swing.JFrame {
         };*/
          
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        dispose();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
